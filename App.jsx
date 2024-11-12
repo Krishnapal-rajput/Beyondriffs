e02df6ff4be7c6f95d50d64f7b43ca679454ad4a
Krishnapal Rajput <krishnapalraj07@gmail.com>
2024-11-12 17:55:57 +0530

    add file

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/templates/Header.jsx";
+import Main from "./components/templates/Main.jsx";
+import Footer from "./components/templates/Footer.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
